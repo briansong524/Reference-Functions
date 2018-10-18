e35e3308fef37167a0578702461b7690119b2260
Brian Song <44029531+briansong524@users.noreply.github.com>
2018-10-18 09:41:34 -0700

    added decorator function

--- general_functions.py
+++ general_functions.py
@@ -2,3 +2,15 @@
 
 import os
 os.environ["CUDA_VISIBLE_DEVICES"]="0" # this might be just for nvidia
+
+### Decorator for printing time (DPT) ###
+
+def my_timer(function_):
+    import time
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = function_(*args,**kwargs)
+        time_taken = time.time() - start_time
+        print('{}() ran in: {} seconds.'.format(function_.__name__, time_taken))
+        return result
+    return wrapper
