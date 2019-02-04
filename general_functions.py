62da33d9b5b4123f7f628aee8dad3c637d3aab7b
Brian Song <44029531+briansong524@users.noreply.github.com>
2019-02-04 11:24:59 -0800

    added CATIND

--- general_functions.py
+++ general_functions.py
@@ -63,3 +63,40 @@ class my_timer:
 # timer.print_reset('wait three seconds later') # returns "time to wait three seconds later: 0 hour(s) 0 minute(s) 3 second(s)."
 # time.sleep(1)
 # timer.time_from_init() # returns "total time since starting time: 0 hour(s) 0 minute(s) 5 second(s)."
+
+
+### class for indexing categorical variables (CATIND) ### 
+
+class categorical_dictionary:
+    'class containing all categorical variables indexed and converted'
+    
+    def __init__(self):
+        self.cat_dict = {}
+        self.rev_cat_dict = {}
+    
+    def add_col(self, vals, col_name, verbose = True):
+        cat_vals = set(vals) # get classes
+        temp_dict = dict(zip(cat_vals, range(1, len(cat_vals)+1)))
+        temp_dict[col_name + '_UNK'] = 0 # adding an index for previously non-existant class 
+        self.cat_dict[col_name] = temp_dict
+        rev_temp_dict = {j:i for i,j in temp_dict.items()}
+        self.rev_cat_dict[col_name] = rev_temp_dict
+        if verbose:
+            print('Added ' + col_name)
+            
+    def cat_to_ind(self, vals, col_name):
+        def failsafe_mapper(val, col_name):
+            'make mapping robust by handling previously unseen classes'
+            try:
+                mapped_val = self.cat_dict[col_name][val]
+            except:
+                print('Unknown value: "' + str(val) + '", appending as index 0 (general unknown class index)')
+                mapped_val = 0
+            return mapped_val
+        
+        mapped_list = list(map(lambda x: failsafe_mapper(x,col_name), vals))
+        return mapped_list
+    
+    def ind_to_cat(self, vals, col_name):
+        return list(map(lambda x: self.rev_cat_dict[col_name][x], vals))
+    
