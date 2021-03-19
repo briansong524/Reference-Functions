69f7043f567b0911c6f45d1f83a8071646e2a9da
Brian Song <44029531+briansong524@users.noreply.github.com>
2021-03-18 20:11:12 -0700

    Create cv_utils.py

---
+++ cross_validation/cv_utils.py
@@ -0,0 +1,32 @@
+def cv():
+
+    ## data manipulation
+
+    ## setup train data
+    # define 'label' as y, everything else as x
+
+    train_df
+
+    ## setup cross validation parameters
+
+    ## display results
+
+    ## output test set results (if needed)
+
+
+def cv_configs():
+    # All the necessary parameters and defined functions involved in running 
+    # this cross-validation function.
+    # 
+    # Can move this function to a separate file for easier editing.
+    
+    params = {
+        
+    }
+    return params
+
+def data_manipulation(df):
+    '''
+    Skeleton of a standard data manipulation method. Use this to 
+    
+    '''
